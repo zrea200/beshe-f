333d5f64e46ed863df262df7ab11d9753002870a
zreal <33@cc.com>
2025-02-16 16:11:22 +0800

    refactor(layout): 调整底部导航栏样式和位置
    - 注释掉 BasicLayout.vue 中的 @change 事件处理函数
    - 修改 myAxios.ts 中的请求和响应拦截器日志输出

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosInstance} from "axios";
+import axios, { AxiosInstance } from "axios";
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -10,7 +10,7 @@ myAxios.defaults.withCredentials = true; // 配置为true
 
 // Add a request interceptor
 myAxios.interceptors.request.use(function (config) {
-    console.log('我要发请求啦', config)
+    // console.log('我要发请求啦', config)
     // Do something before request is sent
     return config;
 }, function (error) {
@@ -20,7 +20,7 @@ myAxios.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 myAxios.interceptors.response.use(function (response) {
-    console.log('我收到你的响应啦', response)
+    // console.log('我收到你的响应啦', response)
     // 未登录则跳转到登录页
     if (response?.data?.code === 40100) {
         const redirectUrl = window.location.href;
